c85128b1ef2af4ad4c6601cbb625a5a6db6785a8
root <Rehmhakeem.official@gmail.com>
2023-10-20 12:20:39 -0700

    all

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,53 @@
+#include "mainn.h"
+
 #include "main.h"
 /**
- * _pow_recursion - Returns the value of x raised to the power of y
- * @x: the value to multiply
- * @y: the times to multiply the value
- *
- * Return: the value multiplied y times
- */
-int _pow_recursion(int x, int y)
+  * _lenght - checks the length of a string
+  * @s: is the string
+  * Return: return the leng of the string
+**/
+int _lenght(char *s)
 {
-if (y < 0)
-return (-1);
+	if (*s == '\0')
+		return (0);
+	return (1 + _lenght(s + 1));
+}
 
-if (y == 0)
-return (1);
+/**
+ * checkp - checks if the string is palindrome
+ * @i: is the index
+ * @lg: is the length of the string
+ * @s: is the string
+ * Return: 1 if is polindrome or 0 if not
+**/
+int checkp(int i, int lg, char *s)
+{
+	if (lg > 0)
+	{
+		if (s[i] == s[lg])
+		{
+			return (checkp(i + 1, lg - 1, s));
+		}
+		else if (s[i] != s[lg])
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	return (1);
+}
 
-return (x * _pow_recursion(x, y - 1));
+
+/**
+  * is_palindrome - Checks if a string is a palindrome
+  * @s: is the string
+  * Return: return 1 if the string is a palindrome or 0 otherwise
+**/
+int is_palindrome(char *s)
+{
+	return (checkp(0, _lenght(s) - 1, s));
 }
 
