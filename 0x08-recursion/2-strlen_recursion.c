f2ec45b2c7e7b563b18ba90162385137ef7450e2
rehamhkee <rehamhakeem.official@gmail.com>
2023-11-19 07:22:12 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /*
  * Write a function that returns... 
  * the length of a string
  */
+
 int _strlen_recursion(char *s)
+
 	{
 	if (*s == '\0')
 		return (0);
 	return (_strlen_recursion(s + 1) + 1);
 }
-
