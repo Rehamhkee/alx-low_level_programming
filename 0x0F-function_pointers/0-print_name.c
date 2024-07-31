006172009b596dfa56e7d9ddce8bbb8d942bf658
root <rehamhakeem.official@gmail.com>
2024-07-31 06:39:09 -0700

    first task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,27 +1,17 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing function
- */
-void print_name(char *name, void (*f)(char *))
-{
-	if (!name || !f)
-		return;
-	f(name);
-#include <stdlib.h>
-#include "function_pointers.h"
+*print_name - prints a name
+*@name:name to print
+*@f: function which print name
+*/
 
-/**
- * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing function
- */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
-}}
+
+if (name == NULL || f == NULL)
+return;
+
+f(name);
+
+}
